9205ed4d856e15cd4d540b93536701f57c31dafd
rlghddlrlghd <rlghddlrlghd@gmail.com>
2026-01-30 05:07:06 +0000

    feat: Add dark mode toggle

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Add JS here
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+    }
+
+    themeToggle.addEventListener('click', () => {
+        if (document.body.classList.contains('dark-mode')) {
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light-mode');
+        } else {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark-mode');
+        }
+    });
+});
